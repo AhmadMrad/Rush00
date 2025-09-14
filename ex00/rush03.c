c094c4cf39bf50fa56b350a1cf79bb834f5f2ac7
Ahmad_Mrad <Ahmadmrad169@gmail.com>
2025-09-14 18:57:16 +0300

    After changes

--- ex00/rush03.c
+++ ex00/rush03.c
@@ -1,21 +1,21 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   rush03.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/14 14:28:17 by marvin            #+#    #+#             */
-/*   Updated: 2025/09/14 15:40:00 by marvin           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-void	ft_putchar(char c);
+void	print_char(int row, int col, int x, int y)
+{
+	if ((row == 0 && col == 0) || (row == y - 1 && col == 0))
+		ft_putchar('A');
+	else if ((row == 0 && col == x - 1) || (row == y - 1 && col == x - 1))
+		ft_putchar('C');
+	else if (row == 0 || row == y - 1)
+		ft_putchar('B');
+	else if (col == 0 || col == x - 1)
+		ft_putchar('B');
+	else
+		ft_putchar(' ');
+}
 
 void	rush(int x, int y)
 {
-	int	row;
-	int	col;
+	int row;
+	int col;
 
 	if (x <= 0 || y <= 0)
 		return ;
@@ -25,16 +25,7 @@ void	rush(int x, int y)
 		col = 0;
 		while (col < x)
 		{
-			if ((row == 0 && col == 0) || (row == y - 1 && col == 0))
-				ft_putchar('A');
-			else if ((row == 0 && col == x - 1) || (row == y - 1 && col == x - 1))
-				ft_putchar('C');
-			else if (row == 0 || row == y - 1)
-				ft_putchar('B');
-			else if (col == 0 || col == x - 1)
-				ft_putchar('B');
-			else
-				ft_putchar(' ');
+			print_char(row, col, x, y);
 			col++;
 		}
 		ft_putchar('\n');
